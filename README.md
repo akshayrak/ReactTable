7378b665aea0e745e27ce3350cf5bb6e41500225
akshayrak <akshayrak77@gmail.com>
2021-12-18 21:21:16 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# react-jtgklu
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-jtgklu)
---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+import "./style.css";
+import {BasicTable} from './components/BasicTable'
+
+export default function App() {
+  return (
+    <div>
+      <BasicTable />
+    </div>
+  );
+}
